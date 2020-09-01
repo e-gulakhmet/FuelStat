47d39df3783eab6c12fa07d28af8729189ef131b
Enes <enes515254@gmail.com>
2020-09-01 23:14:54 +0600

    Fix data base insert function

--- database.py
+++ database.py
@@ -13,24 +13,13 @@ class DataBase():
             self.logger.info("Connecting to database...")
             self.db = sqlite3.connect("data/" + database_name + ".db")
             self.logger.info("Connected to database file")
-            self.logger.info("Creating cursor...")
-            self.dbc = self.db.cursor()
-            self.logger.info("DataBase cursor was created")
         except sqlite3.Error as e:
             self.logger.error(e)
-
-        # self.logger.info("Creating cursor...")
-        # # Создаем курсор дл управления базой данных
-        # try:
-
-        # except sqlite3.Error as e:
-        #     self.logger.error(e)
-
 
     def create_table(self, colums_name): # Создать таблицу
         self.logger.info("Creating table...")
         try:
-            self.dbc.execute("CREATE TABLE IF NOT EXISTS " + self.database_name + " (" + colums_name + ")")
+            self.db.execute("CREATE TABLE IF NOT EXISTS " + self.database_name + " (" + colums_name + ")")
             self.logger.info("Table was created")
         except sqlite3.Error as e:
             self.logger.warning(e)
@@ -40,34 +29,15 @@ class DataBase():
         self.logger.info("Inserting data into the table...")
         try:
             if colums is None:
-                self.dbc.execute("INSERT INTO " + self.database_name + " VALUES (" + data + ")")
+                self.db.execute("INSERT INTO " + self.database_name + " VALUES (" + data + ")")
             else:
-                self.dbc.execute("INSERT INTO " + self.database_name + "(" + colums + ") VALUES (" + data + ")")
+                self.db.execute("INSERT INTO " + self.database_name + "(" + colums + ") VALUES (" + data + ")")
             self.logger.info("Data was added to the table")
         except sqlite3.Error as e:
             self.logger.warning(e)
     
     def insert_list(self, colums=None, data=None): # Вставить списки со значениями в таблицу
         self.logger.info("Inserting listdata into the table...")
-        # # try:
-        # #     d = ""
-        # #     i = 0
-        # #     for s in data:
-        # #         r = ""
-        # #         g = 0
-        # #         for t in s:
-        # #             if g == 0:
-        # #                 r = "( " + t
-        # #             else:
-        # #                 r += ", " + t
-        # #             g += 1
-        # #         if i == 0:
-        # #             d = r + ")"
-        # #         else:
-        # #             d += ", " + t + ") "
-        # #         i +=1
-            
-        #     print(d)
         try:
             s = ""
             for r in range(0, len(data[0])):
@@ -75,17 +45,10 @@ class DataBase():
                     s += "?,"
                 else:
                     s += "?"
-            # for r in range(0, len(data)):
-            #     if r < len(data) - 1:
-            #         s += "?," 
-            #     else:
-            #         s += "?"
-            print(s)
-            
             if colums is None:
-                self.dbc.execute("INSERT INTO " + self.database_name + " VALUES (" + s + ')', data)
+                self.db.execute("INSERT INTO " + self.database_name + " VALUES (" + s + ')', data)
             else:
-                self.dbc.execute("INSERT INTO " + self.database_name + '(' + colums + ") VALUES (" + s + ')', data)
+                self.db.executemany("INSERT INTO " + self.database_name + "(" + colums + ") VALUES (" + s + ")", data)
             self.logger.info("Data was added to the table")
         except sqlite3.Error as e:
             self.logger.warning(e)
@@ -95,11 +58,11 @@ class DataBase():
         self.logger.info("Selecting data from the table...")
         try:
             if condition is None:
-                self.dbc.execute("SELECT " + data + " FROM " + self.database_name)
+                c = self.db.execute("SELECT " + data + " FROM " + self.database_name)
                 self.logger.info("Data was selected from table")
             else:
-                self.dbc.execute("SELECT " + data + " FROM " + self.database_name + " WHERE " + condition)
+                c = self.db.execute("SELECT " + data + " FROM " + self.database_name + " WHERE " + condition)
                 self.logger.info("Data was selected from table with condition")
-            return self.dbc.fetchone()
+            return c
         except sqlite3.Error as e:
             self.logger.warning(e)
--- main.py
+++ main.py
@@ -23,20 +23,24 @@ def main():
 
     fuel.create_table('''id INTEGER PRIMARY KEY,
                          name TEXT NOT NULL,
-                         price REAL NOT NULL''')
+                         price INTEGER NOT NULL''')
 
-    f = open("data/fuel.txt")
+    fuel_txt = open("data/fuel.txt", 'r')
+    f = fuel_txt.read() 
+    fuels = f.split("\n")
+    # for s in fuels:
+    #     gas = s.split(", ")
+    #     fuel.insert("name, price", "'" + gas[0] + "', " + gas[1])
+
     data = []
-    for line in f:
-        data.append(tuple(line.split(",")))
-    
-    print(data)
-
+    for s in fuels:
+        data.append(tuple(s.split(", ")))
     fuel.insert_list("name, price", data)
 
-    # for row in fuel.select():
-    #     print(row)
-    print(fuel.select())
+    for row in fuel.select():
+        print(row)
+
+    # print(fuel.select())
 
 
     # # Создаем pdf файл с таблицой
