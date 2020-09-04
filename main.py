067b1658dafa4eb14bd0e54b6aa53a90f3a65f3a
who-man-tech <enes515254@gmail.com>
2020-09-04 08:49:24 +0600

    Udpate parser aguments

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import database
 
 
 # TODO: Дополнить документацию
-# TODO: Добавить параметры при запуске
 # TODO: Задокументировать txt файлы
 
 
@@ -16,36 +15,36 @@ def main():
                                      description="""Creating statistics 
                                                     based on generated tables""")
 
-    parser.add_argument("--recreate", action="store_true",
+    parser.add_argument("-r", "--recreate", action="store_true",
                         help="re-create all data base")
+    parser.add_argument("--log", action="store", default="info",
+                        help="enable logging")
+    
     args = parser.parse_args()
 
-# TODO: Переконвертировать данные из TEXT в INTEGER
+
     logging.basicConfig(filename="logging.log",
-                        level=logging.DEBUG,
+                        level=args.log.upper(),
                         format="%(asctime)s %(name)s [%(levelname)s] : %(message)s")
     logger = logging.getLogger("MAIN")
 
+
     # Создаем базу данных заправок
     db = database.DataBase("data/database.db")
-
+    # Создаем таблицу запрвавок
     db.create_table("fuel",
                     '''id INTEGER PRIMARY KEY,
                        name TEXT NOT NULL,
                        price INTEGER NOT NULL''',
                     args.recreate)
-
-    # for s in dbs:
-    #     gas = s.split(", ")
-    #     db.insert("name, price", "'" + gas[0] + "', " + gas[1])
-
+    # Вставляем данные из текстового файла в таблицу
     db.insert_list("fuel", "name, price", txt_to_list("data/fuel.txt"))
 
     for row in db.select("fuel"):
         print(row)
     print("\n")
 
-
+    # Создаем таблицу транзакций
     db.create_table("trans",
                     """id INTEGER PRIMARY KEY,
                        dtime TEXT DEFAULT CURRENT_TIMESTAMP,
@@ -54,14 +53,14 @@ def main():
                        amount INTEGER NOT NULL,
                        FOREIGN KEY (fuel_id) REFERENCES fuel(id)""",
                     args.recreate)
-
+    # Вставляем данные из текстового файла в таблицу
     db.insert_list("trans", "dtime, odometer, fuel_id, amount", txt_to_list("data/trans.txt"))
 
     for row in db.select("trans"):
         print(row)
     print("\n")
 
-    # Объединяем таблицы
+    # Объединяем данные из таблиц
     c = db.select("trans t, fuel f",
                   "t.dtime, f.name, f.price, t.odometer, t.amount, t.amount * f.price AS cost",
                   "t.fuel_id = f.id")
