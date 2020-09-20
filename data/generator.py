a1a5c768255b67179aba5a44ef4a722fcce1ca66
who-man-tech <enes515254@gmail.com>
2020-09-20 09:55:24 +0600

    Remove argparse

--- data/generator.py
+++ data/generator.py
@@ -1,17 +1,9 @@
 import csv
-import argparse
 import random
 
 
 
 def main():
-    parser = argparse.ArgumentParser(prog="fuel_stat",
-                                     description="""Creating statistics 
-                                                    based on generated tables""")
-    parser.add_argument("--range", action="store",
-                        help="")
-    args = parser.parse_args()
-
     gas_name = ["Texaco", "Arco", "Shell", "Mobil",
                 "Chevron", "DX", "Conoco", "American",
                 "Phillips", "Ericson", "Exxon"]
@@ -40,13 +32,12 @@ def main():
         odometer += random.randint(50, 600)
         name = gas_name[random.randint(0, len(gas_name) - 1)]
         amount = random.uniform(0, 15)
-        print(i)
         lst.append({"dtime": str(y) + '-' + str(m) + '-' + str(d),
                     "odometer": odometer,
-                    "name": "Texaco",
+                    "name": name,
                     "amount": amount})
 
-    with open('test.csv', 'w', newline='') as csvfile:
+    with open('trans.csv', 'w', newline='') as csvfile:
         # Задаем столбцы, в которые будем заносить данные
         fieldnames = ["dtime", "odometer", "name", "amount"]
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
