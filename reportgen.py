f39f9588baefa57442eeaa077ef5759182b2dae9
who-man-tech <enes515254@gmail.com>
2020-09-30 10:06:36 +0600

    Add transfering table to the next page

--- reportgen.py
+++ reportgen.py
@@ -4,11 +4,10 @@ from reportlab.pdfgen import canvas
 from reportlab.lib.units import mm
 from reportlab.lib import colors
 from reportlab.lib.pagesizes import A4
-from reportlab.platypus import Table, TableStyle
+from reportlab.platypus import Table, TableStyle, SimpleDocTemplate, Paragraph
+from reportlab.lib.styles import getSampleStyleSheet
 
 
-
-# TODO: Сделать переход на следующую страницу, если данных слишком много
 # TODO: Добавить в таблицу данные о цене одного дня и пробеге между заправками
 # TODO: Добавить статистику по выгодности заправок
 # TODO: Добавить информацию о самой часто используемой заправке
@@ -50,7 +49,6 @@ def report(start_date=None, end_date=None, gas_names=None, file_name=None):
                       AND tt.id = (SELECT MAX(id) FROM trans WHERE id < t.id)
                       ORDER BY t.dtime""")
 
-    logger.info("Report data was selected from database")
 
     # Получаем данные для таблицы из базы дыннах
     # Создаем строку условия для select
@@ -66,29 +64,15 @@ def report(start_date=None, end_date=None, gas_names=None, file_name=None):
 
     # Создаем pdf файл
     w, h = A4 # Размер листа
-    pdf = canvas.Canvas("data/" + file_name + ".pdf", A4)
 
-    # Рисуем заголовок
-    pdf.drawString(82 * mm, h - 8.4 * mm, "FuelStat Report")
-    
-    # Рисуем параметры полученной таблицы
-    pdf.drawString(8 * mm, h - 29 * mm, "Start Date: " + start_date)
-    pdf.drawString(60 * mm, h - 29 * mm, "End Date: " + end_date)
-    gs = ""
-    # Создаем строку с названями заправки
-    if gas_names is None:
-        gs = "All"
-    else:
-        gs = str(gas_names[0])
-        for n in gas_names:
-            gs += ", " + str(n)
-    # Выводим созданную строку
-    pdf.drawString(110 * mm, h - 29 * mm, "Gas Stations: " + gs)
+    # Cоздаем документ, в котором будут содержать полученные данные
+    doc = SimpleDocTemplate("data/" + file_name + ".pdf", pagesize=A4)
+    elements = [] # Список, который будет содержать все элементы документа
+    styles = getSampleStyleSheet()
 
-    pdf.setLineWidth(1 * mm)
-    pdf.line(8 * mm, h - 35 * mm, 200 * mm, h - 35 * mm)
+    p = Paragraph("FuelStat", styles["Heading1"])
+    elements.append(p)
 
-    # Создаем таблицу
     # Получаем данные из базы данных
     table_data = table_data_to_list(db.select("v_trans", 
                                               """dtime, name,
@@ -99,25 +83,71 @@ def report(start_date=None, end_date=None, gas_names=None, file_name=None):
     table_data.insert(0, 
                       ["DATE", "GAS", "ODOMETER", "GALLON \n PRICE",
                        "GALLONS", "COST", "MPG", "MILE \n PRICE"])
-    # Проверяем, если данные слишком много,
-    # то переносим оставшиеся данные на следующую страницу
-    t = Table(table_data)
-    t_w, t_h = t.wrap(0, 0)
-    t.wrapOn(pdf, w, h)
+
+    # Создаем таблицу
+    t = Table(table_data, repeatRows=1)
     t.setStyle(TableStyle([
-        ("BACKGROUND", (0, 0), (-1, 0), colors.lightblue),
-        ('INNERGRID', (0, 0), (-1, -1), 0.25, colors.black),
-        ('BOX', (0, 0), (-1, -1), 0.25, colors.black)
-    ]))
-    # Выводим таблицу
-    t.drawOn(pdf, 9 * mm, h - 42 * mm - t_h)
+                          ("BACKGROUND", (0, 0), (-1, 0), colors.lightblue),
+                          ('INNERGRID', (0, 0), (-1, -1), 0.25, colors.black),
+                          ('BOX', (0, 0), (-1, -1), 0.25, colors.black)
+                          ]))
+    
+    elements.append(t)
+    
+    doc.build(elements)
 
-    pdf.showPage()
+    # pdf = canvas.Canvas("data/" + file_name + ".pdf", A4)
 
-    pdf.drawString(0, 0, "Hello")
+    # # Рисуем заголовок
+    # pdf.drawString(82 * mm, h - 8.4 * mm, "FuelStat Report")
+    
+    # # Рисуем параметры полученной таблицы
+    # pdf.drawString(8 * mm, h - 29 * mm, "Start Date: " + start_date)
+    # pdf.drawString(60 * mm, h - 29 * mm, "End Date: " + end_date)
+    # gs = ""
+    # # Создаем строку с названями заправки
+    # if gas_names is None:
+    #     gs = "All"
+    # else:
+    #     gs = str(gas_names[0])
+    #     for n in gas_names:
+    #         gs += ", " + str(n)
+    # # Выводим созданную строку
+    # pdf.drawString(110 * mm, h - 29 * mm, "Gas Stations: " + gs)
 
-    pdf.save()
-    logger.info("Report was saved")
+    # pdf.setLineWidth(1 * mm)
+    # pdf.line(8 * mm, h - 35 * mm, 200 * mm, h - 35 * mm)
+
+    # # Создаем таблицу
+    # # Получаем данные из базы данных
+    # table_data = table_data_to_list(db.select("v_trans", 
+    #                                           """dtime, name,
+    #                                              odometer, gallon_price,
+    #                                              amount, cost, mpg,
+    #                                              mile_price""",
+    #                                           condition))
+    # table_data.insert(0, 
+    #                   ["DATE", "GAS", "ODOMETER", "GALLON \n PRICE",
+    #                    "GALLONS", "COST", "MPG", "MILE \n PRICE"])
+    # # Проверяем, если данные слишком много,
+    # # то переносим оставшиеся данные на следующую страницу
+    # t = Table(table_data, repeatRows=1)
+    # t_w, t_h = t.wrap(0, 0)
+    # t.wrapOn(pdf, w, h)
+    # t.setStyle(TableStyle([
+    #     ("BACKGROUND", (0, 0), (-1, 0), colors.lightblue),
+    #     ('INNERGRID', (0, 0), (-1, -1), 0.25, colors.black),
+    #     ('BOX', (0, 0), (-1, -1), 0.25, colors.black)
+    # ]))
+    # # Выводим таблицу
+    # t.drawOn(pdf, 9 * mm, h - 42 * mm - t_h)
+
+    # pdf.showPage()
+
+    # pdf.drawString(0, 0, "Hello")
+
+    # pdf.save()
+    # logger.info("Report was saved")
 
 
 def table_data_to_list(data):
