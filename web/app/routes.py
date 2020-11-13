0074fa648df4df8fde959885ab8929d4648064bd
who-man-tech <enes515254@gmail.com>
2020-11-13 10:41:25 +0600

    Add login error

--- web/app/routes.py
+++ web/app/routes.py
@@ -1,20 +1,39 @@
+# Данные файл содержит в себе функции для отображения страниц, по
+# указанным путям
+
+
 from app import flsk
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 from app.forms import LoginForm
 
+user = {"username": "operator", "password": "operator"}
 
-@flsk.route('/', methods=['GET', 'POST'])
+
+@flsk.route("/")
+@flsk.route("/index")
+def index():
+    return render_template("index.html", title="Index", user=user)
+
+
+@flsk.route("/login", methods=["GET", "POST"])
 def login():
-    # При входе на сайт, пользователя сразу переходит к странице
+    # При входе на сайт, пользователь сразу переходит к странице
     # авторизации.
     # Он вводит свой логин и пароль, а так как пользователь у нас один,
     # то проверяем введенные данные с данными, которые у нас есть.
     # Если были введены верные данные, то переводим его на основную
     # страницу.
     # Иначе просим его ввести данные снова.
-    user = {"name": "operator", "password": "operator"}
+
+    # Создаем объект form, который содержит в себе веб-формы для
+    # авторизации
     form = LoginForm()
+    # Если пришел POST запрос от браузера
     if form.validate_on_submit():
-        flash('Login requested for user {}'.format(
+        # Говорим, какой логин был получен
+        flash("Login requested for user {}".format(
             form.username.data))
+        # Переходим к другой странице
+        return redirect(url_for("index"))
+    # Генерируем страницу авторизиции
     return render_template('login.html', title='Login', user=user, form=form)
