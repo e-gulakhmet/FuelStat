3d88ee61535a485fc143d6f7ffe147fc5670e762
who-man-tech <enes515254@gmail.com>
2020-11-16 01:07:58 +0600

    Add bootstrap

--- web/app/__init__.py
+++ web/app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_login import LoginManager
+from flask_bootstrap import Bootstrap
 
 flsk = Flask(__name__)
 flsk.config["SECRET_KEY"] = "pedaling"
@@ -8,5 +9,8 @@ login = LoginManager(flsk)
 # Страница, которую получит пользователь, если не авторизуется,
 # но захочет зайти на страницу для зарегистированных пользователей
 login.login_view = 'login'
+login.login_message = "Please Sign In"
+
+bootstrap = Bootstrap(flsk)
 
 from app import routes
