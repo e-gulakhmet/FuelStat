f071527511865391bf4c004f06fbd10412b2ea82
who-man-tech <enes515254@gmail.com>
2020-12-30 11:50:27 +0200

    Add javascript

---
+++ web/app/static/javascript/main.js
@@ -0,0 +1,10 @@
+function editRow() {
+     date = document.getElementsByClassName("row_form date")
+    let odometer = document.getElementsByClassName("row_form odometer")
+    let station = document.getElementsByClassName("row_form fuel_station")
+    let gallon = document.getElementsByClassName("row_form gallon_count")
+
+    const header = document.getElementsByClassName("navig_header")
+    header.style.backgroundColor = "black"
+    console.log(header)
+}
