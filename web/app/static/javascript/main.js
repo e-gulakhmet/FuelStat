f1008334d17ef2415b64e61b73508e5060bca22f
who-man-tech <enes515254@gmail.com>
2020-12-30 13:26:35 +0200

    Update javascript editRow function

--- web/app/static/javascript/main.js
+++ web/app/static/javascript/main.js
@@ -1,10 +1,11 @@
-function editRow() {
-     date = document.getElementsByClassName("row_form date")
-    let odometer = document.getElementsByClassName("row_form odometer")
-    let station = document.getElementsByClassName("row_form fuel_station")
-    let gallon = document.getElementsByClassName("row_form gallon_count")
-
-    const header = document.getElementsByClassName("navig_header")
-    header.style.backgroundColor = "black"
-    console.log(header)
+function editRow(row_id) {
+    let date = document.querySelector(".row_form.date")
+    let odometer = document.querySelector(".row_form.odometer")
+    let station = document.querySelector(".row_form.fuel_station")
+    let gallon = document.querySelector(".row_form.gallon_count")
+    date.style.display = "block"
+    odometer.style.display = "block"
+    station.style.display = "block"
+    gallon.style.display = "block" 
+    console.log(row_id)   
 }
