3536faebeafd39920fc01d7f624fcd9adba2ab1a
who-man-tech <enes515254@gmail.com>
2021-01-03 12:32:30 +0200

    Update workspace form's data pusher

--- web/app/static/javascript/main.js
+++ web/app/static/javascript/main.js
@@ -8,26 +8,30 @@ function editRow(row_id, station_id) {
         var form_cols = work_row.querySelectorAll(".form_col")
         var table_cols = work_row.querySelectorAll(".table_col")
         var fuel_data = document.querySelector(".navig_form.names").querySelectorAll("option")
+        var form_names = [".date", ".odometer", ".station", ".gallons"]
+
         form_cols[0].querySelector(".id").defaultValue = row_id
-        form_cols[1].querySelector(".date").defaultValue = table_cols[0].innerHTML
-        form_cols[2].querySelector(".odometer").defaultValue = table_cols[1].innerHTML
-        var option = document.createElement("option")
-        option.value = station_id
-        option.text = table_cols[2].innerHTML
-        form_cols[3].querySelector(".station").add(option)
-        for (var i = 0; i < fuel_data.length; i++) {
-            console.log(fuel_data[i])
-            var opt = document.createElement("option")
-            if ( fuel_data[i].value == station_id) {
-                continue
+        for (var i = 1; i < form_cols.length - 1; i++) {
+            form_cols[i].querySelector(form_names[i - 1]).defaultValue = table_cols[i - 1].innerHTML
+            if (i == 3) {
+                var option = document.createElement("option")
+                option.value = station_id
+                option.text = table_cols[i - 1].innerHTML
+                form_cols[i].querySelector(form_names[i - 1]).add(option)
+                for (var t = 0; t < fuel_data.length; t++) {
+                    var opt = document.createElement("option")
+                    if ( fuel_data[t].value == station_id) {
+                        continue
+                    }
+                    else {
+                        opt.value = fuel_data[t].value
+                        opt.text = fuel_data[t].text
+                    }
+                    form_cols[i].querySelector(form_names[i - 1]).add(opt)
+                }
             }
-            else {
-                opt.value = fuel_data[i].value
-                opt.text = fuel_data[i].text
-            }
-            form_cols[3].querySelector(".station").add(opt)
+            form_cols[i].querySelector(form_names[i - 1]).defaultValue = table_cols[i - 1].innerHTML
         }
-        form_cols[4].querySelector(".gallons").defaultValue = table_cols[3].innerHTML
         for (var i = 1; i < form_cols.length; i++) {
             form_cols[i].style.display = "table-cell"
             table_cols[i - 1].style.display = "none"
