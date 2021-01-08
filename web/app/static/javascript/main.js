3e5659a761c51cf61a6e7c09539f5992cf0a3661
who-man-tech <enes515254@gmail.com>
2021-01-08 12:31:07 +0200

    Add date valid function

--- web/app/static/javascript/main.js
+++ web/app/static/javascript/main.js
@@ -93,16 +93,59 @@ function newRow() {
 }
 
 
-function validOdometer(odometer) {
-    form_date = document.querySelector(".work_table_row.new .form_col .date").value
-    if (date.value != "") {
-        var values = []
-        var max_values = []
-        var min_values = []
+function validDateNewRow(date) {
+    form_odometer = document.querySelector(".work_table_row.new .form_col .odometer").value
+    if (form_odometer != "") {
+        var max_value = ""
+        var min_value = ""
         var error = ""
         rows = document.querySelectorAll(".work_table_row")
         for (var i = 0; i < rows.length - 1; i++) {
             table_cols = rows[i].querySelectorAll(".table_col")
+            if (table_cols[1].innerHTML < form_odometer) {
+                if (min_value < table_cols[0].innerHTML) {
+                    min_value = table_cols[0].innerHTML
+                }
+            }
+            else if (table_cols[1].innerHTML > form_odometer) {
+                console.log(table_cols[1])
+                if (max_value > table_cols[0].innerHTML || max_value == "") {
+                    max_value = table_cols[0].innerHTML
+                }
+            }
+        }
+        if (date.value <= min_value || date.value >= max_value) {
+            error = "Date must be more than " + min_value + " and less than " + max_value
+        }
+        console.log(date.value)
+        console.log(min_value)
+        console.log(max_value)
+        if (error != "") {
+            document.querySelector(".error.new_row").innerHTML = "* " + error
+            date.style.borderColor = "red"
+        }
+        else {
+            document.querySelector(".error.new_row").innerHTML = ""
+            date.style.borderColor = "green"
+        }
+    }
+}
+
+
+function validOdometerNewRow(odometer) {
+    form_date = document.querySelector(".work_table_row.new .form_col .date").value
+    var values = []
+    var max_values = []
+    var min_values = []
+    var error = ""
+    rows = document.querySelectorAll(".work_table_row")
+    for (var i = 0; i < rows.length - 1; i++) {
+        table_cols = rows[i].querySelectorAll(".table_col")
+        if (odometer.value == table_cols[1].innerHTML) {
+            error = "Odometer must be different from the existing one"
+            break
+        }
+        if (form_date != "") {
             if (form_date == table_cols[0].innerHTML) {
                 values.push(parseInt(table_cols[1].innerHTML))
             }
@@ -113,22 +156,24 @@ function validOdometer(odometer) {
                 max_values.push(parseInt(table_cols[1].innerHTML))
             }
         }
+    }
+    if (error == "") {
         if (values.length == 1 && odometer.value == values[0]) {
             error = "Odometer must be more or less than " + values[0]
         }
-        else if (values.length > 1 && (odometer.value < Math.min.apply(null, values) || odometer.value > Math.max.apply(null, values))) {
+        else if (values.length > 1 && (odometer.value <= Math.min.apply(null, values) || odometer.value >= Math.max.apply(null, values))) {
             error = "Odometer must be more than " + Math.min.apply(null, values) + " and less than " + Math.max.apply(null, values)
         }
-        else if (odometer.value < Math.max.apply(null, min_values) || odometer.value > Math.min.apply(null, max_values)) {
+        else if (odometer.value <= Math.max.apply(null, min_values) || odometer.value >= Math.min.apply(null, max_values)) {
             error = "Odometer must be more than " + Math.max.apply(null, min_values) + " and less than " + Math.min.apply(null, max_values)
         }
-        if (error != "") {
-            document.querySelector(".error.new_row").innerHTML = "* " + error
-            odometer.style.borderColor = "red"
-        }
-        else {
-            document.querySelector(".error.new_row").innerHTML = ""
-            odometer.style.borderColor = "#808080"
-        }
+    }
+    if (error != "") {
+        document.querySelector(".error.new_row").innerHTML = "* " + error
+        odometer.style.borderColor = "red"
+    }
+    else {
+        document.querySelector(".error.new_row").innerHTML = ""
+        odometer.style.borderColor = "green"
     }
 }
