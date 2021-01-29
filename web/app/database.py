f4b1cfca2585976e9001c6db2db4b73a69fdb14c
who-man-tech <enes515254@gmail.com>
2021-01-29 11:03:18 +0200

    Update database functions and add them to routes

---
+++ web/app/database.py
@@ -0,0 +1,368 @@
+import sqlite3
+import logging
+import os
+import csv
+
+
+# TODO: Добавить функцию отсуствия названия таблицы в select
+
+
+class DataBase():
+    """
+    Класс создания и настройки базы данных.
+
+    """
+
+
+    def __init__(self, database_file, re_create=False):
+        """
+        Инициализация базы данных.
+        Подлкючаемся к базе данных.
+
+        Parameters
+        ----------
+        database_file : str
+            Путь к файлу базы данных
+        re_create : bool
+            Если данный флаг активен, база данных пересоздатся.
+            В итоге будет пустая база данных, без таблиц.
+
+        """
+
+        self.logger = logging.getLogger("DATABASE")
+
+        self.connected = False
+
+        # Удаляем файл базу данных
+        self.logger.info("Deleting table...")
+        if re_create:
+            try:
+                os.remove(database_file)
+                self.logger.info("Database was deleted")
+            except FileNotFoundError:
+                self.logger.info("Database not found")
+
+
+        # Подключаемся к базе данных
+        self.logger.debug("Connecting to database...")
+        try:
+            self.db = sqlite3.connect(database_file)
+            self.connected = True
+            self.logger.info("Connected to database file(" + database_file + ')')
+        except sqlite3.Error as e:
+            self.logger.error(e)
+
+    def create_table(self, table_name, colums_name): # Создать таблицу
+        """
+        Создает таблицу исходя из указанной информации.
+        Если при вызове передан txt file, то программа
+        заполнит таблицу данными этого файла.
+
+        Parameters
+        -----------
+        table_name : str
+            Название которое нужно установить для таблицы.
+        colums_name : str
+            Назваения столбцов таблицы.
+            Указываются через запятую.
+            Например: father TEXT, mother TEXT, childcount INT
+        txt_file_path : str
+            Если флаг активен, то данные из этого файла будут
+            сразу загружены в таблицу
+        """
+        if self.connected is True:
+            self.logger.debug("Creating table...")
+            try:
+                self.db.execute("CREATE TABLE IF NOT EXISTS " + table_name + " (" + colums_name + ")")
+                self.logger.info("Table " + table_name + "was created")
+            except sqlite3.Error as e:
+                self.logger.warning(e)
+        else:
+            self.logger.warning("Not connected to database")
+    
+
+    def create_view(self, view_name, table_name, data='*', condition=None, order_by=None, limit=None, re_create=False): # Создать таблицу
+        """
+        Создает вид таблицы(объект созданный на основе другой таблицы)
+        исходя из указанной информации.
+
+        Parameters
+        -----------
+        view_name : str
+            Название которое нужно установить для вида таблица.
+        table_name : str
+            Название таблицы из которой, на основе которых будет
+            создан вид таблицы
+        data : str
+            Данные, которые нужно вставить из таблицы
+            Указываются через запятую.
+            Например: father, mother, childcount
+        condition : str
+            Условие, по которому будут подбираться данные.
+            Например: childcount > 1.
+        order_by : str
+            Элемент таблицы, по которому будет строится последовательность.
+        limit : int
+            Количество строк, которые будут в создаваемой view.
+        re_create : bool
+            Пересоздание таблицы
+        """
+        if self.connected is True:
+            if re_create is True:
+                self.logger.debug("Deleting view...")
+                self.db.execute("DROP VIEW IF EXISTS " + view_name)
+                self.logger.info("View was deleted")
+            self.logger.debug("Creating view...")
+            command = "CREATE VIEW " + view_name + " AS SELECT " + data + " FROM " + table_name
+            if condition is not None:
+                command += " WHERE " + condition
+            try:
+                self.logger.debug(command)
+                self.db.execute(command)
+                self.logger.info("View " + view_name + " was created")
+            except sqlite3.Error as e:
+                self.logger.warning(e)
+        else:
+            self.logger.warning("Not connected to database")
+
+
+    def insert(self, table_name, colums=None, data=None): # Вставить значения в таблицу
+        """
+        Вставляет данные в созданную таблицу.
+
+        Parameters
+        ----------
+        table_name : str
+            Имя таблицы в которую нужно вставить данные.
+        colums : str
+            Имена столбцов в таблицу,
+            в которые нужно будет вставить данные.
+            Записываются через запятую.
+            Например: father, mother, childcount.
+            Если ничего не указывать, то данные
+            присвоятся всем столбцам, взависимости от 
+            последовательности переданных данных.
+        data : str, int
+            Данные, которые вставляются в таблицу.
+            Записываются в порядке столбцов в таблице,
+            либо в порядке столбцов, которые мы указали.
+            Указываются черех запятую.
+            Например, если столбцы равны:
+                father, mother, child count
+            То данные должны быть переданны в таком виде:
+                Jon, Marry, 8
+        """
+        if self.connected:
+            try:
+                command = ""
+                clms_command = ""
+                if colums:
+                    clms_command = "(" + colums + ")"
+                command = "INSERT INTO " + table_name + "(" + colums + ") VALUES (" + data + ")"
+
+                self.logger.debug(command)
+                self.db.execute(command)
+                self.logger.info("Data was added to the table[" + table_name + "]")
+            except sqlite3.Error as e:
+                self.logger.warning(e)
+        else:
+            self.logger.warning("Not connected to database")
+
+    
+    def insert_list(self, table_name, colums=None, data=None): # Вставить списки со значениями в таблицу
+        """
+        Вставляет список с данными в созданную таблицу.
+        Нужно для того, чтобы вствить сразу несолько строк
+        данных при помощи одной команды.
+
+        Parameters
+        ----------
+        table_name : str
+            Имя таблицы в которую нужно вставить данные.
+        colums : str
+            Имена столбцов в таблицу,
+            в которые нужно будет вставить данные.
+            Записываются через запятую.
+            Например: father, mother, childcount.
+            Если ничего не указывать, то данные
+            присвоятся всем столбцам, взависимости от 
+            последовательности переданных данных.
+        data : list
+            Список кортежей, в которых находтся данные вставляемые в таблицу.
+            Записываются в порядке столбцов в таблице,
+            либо в порядке столбцов, которые мы указали.
+            Имеют вид l = [('A', 1), (B, 6)]
+            Например, если столбцы равны:
+                father, mother, child count
+            при этом мы хотим сразу заполнить сразу несколько строк
+            То данные должны быть переданны в таком виде:
+                [("Jon", "Marry", 8), ("Alex", "Lora", 1)]
+        """
+        if self.connected:
+            try:
+                s = ""
+                clms_command = ""
+                for r in range(0, len(data[0])):
+                    if r < len(data[0]) - 1:
+                        s += "?,"
+                    else:
+                        s += "?"
+                if colums:
+                    clms_command = "(" + colums + ")"
+                command = "INSERT INTO " + table_name + clms_command + " VALUES (" + s + ")"
+
+                self.logger.debug(command + " data: " + str(data))
+                self.db.executemany(command, data)
+                self.logger.info("Data was added to the table[" + table_name + "]")
+            except sqlite3.Error as e:
+                self.logger.warning(e)
+        else:
+            self.logger.warning("Not connected to database")
+
+
+    def select(self, table_name, data='*', condition=None, order_by=None, limit=None): # Выделить данные из таблицы
+        """
+        Выделяет данные из таблицы и возвращает их.
+
+        Parameters
+        ----------
+        table_name : str
+            Название таблицы, из которуй нужно
+            достать данные.
+        data : str
+            Названия столбцов, в которых лежат нужные данные.
+            Записываются через запятую.
+            Например: father, mother, childcount.
+            Если данные параметр не указан, то будут получены
+            данные из всех столбов данной таблицы.
+        condition : str
+            Условие, по которому будут подбираться данные.
+            Например: childcount > 1.
+        order_by : str
+            Элемент таблицы, по которому будет строится последовательность.
+        limit : int
+            Количество строк в полученнос списке.
+        
+        Returns
+        -------
+        str
+            Возвращает выделенные данные из таблицы
+            в виде строки или нескольких строк
+        """
+
+        if self.connected:
+            try:
+                command = "SELECT " + data + " FROM " + table_name
+                if condition is not None:
+                    command += " WHERE " + condition
+                if order_by is not None:
+                    command += " ORDER BY " + order_by
+                if limit is not None:
+                    command += " LIMIT " + limit
+
+                self.logger.debug(command)
+                c = self.db.execute(command)
+                self.logger.info("Data was selected from table[" + table_name + "]")
+                return c
+            except sqlite3.Error as e:
+                self.logger.warning(e)
+        else:
+            self.logger.warning("Not connected to database")
+    
+
+    def update(self, table_name: str, data: str, condition: str): # Обновить данные в таблице
+        """
+        Обновляет данные в строках таблице, которые подхотят по условию.
+        
+        Parameters
+        ----------
+        table_name : str
+            Название таблицы, в строках которой нужно обновить данные.
+        data : str
+            Столбцы и данные, которые будут подставлены в эти стролбцы.
+            Пример: "name = 'John', age = 28
+        condition : str
+            Условие, по которому будут подбираться строки,
+            которые нужно изменить.
+        """
+
+        if self.connected:
+            try:
+                command = ("UPDATE" + table_name + "SET " + data +
+                           " WHERE " + condition)
+                self.logger.debug(command)
+                self.db.execute(command)
+                self.logger.info(table_name + "colum's data was updated")
+            except sqlite3.Error as e:
+                self.logger.warning(e)
+        else:
+            self.logger.warning("Not connected to database")
+        
+
+    def delete(self, table_name: str, condition: str): # Удалить данные в таблице
+        """
+        Удаляет строки с данными из таблицы, которые подхотят по условию.
+        
+        Parameters
+        ----------
+        table_name : str
+            Название таблицы, в строках которой нужно обновить данные.
+        condition : str
+            Условие, по которому будут подбираться строки,
+            которые нужно удалить.
+        """
+
+        if self.connected:
+            try:
+                command = ("DELETE FROM" + table_name +
+                           " WHERE " + condition)
+                self.logger.debug(command)
+                self.db.execute(command)
+                self.logger.info("Rows from " + table_name + " were deleted")
+            except sqlite3.Error as e:
+                self.logger.warning(e)
+        else:
+            self.logger.warning("Not connected to database")
+
+    def commit(self):
+        if self.connected:
+            self.logger.debug("Commiting database")
+            try:
+                self.db.commit()
+                self.logger.info("Database was commited")
+            except sqlite3.Error as e:
+                self.logger.warning(e)
+        else:
+            self.logger.warning("Not connected to database")
+
+
+    def disconnect(self):
+        if self.connected:
+            self.logger.debug("Disconnecting from the database")
+            try:
+                self.db.close()
+                self.connected = False
+                self.logger.info("Disconnected from the database")
+            except sqlite3.Error as e:
+                self.logger.warning(e)
+        else:
+            self.logger.warning("Not connected to database")
+        
+    
+    def csv_to_list(self, file_path):
+        if self.connected:
+            self.logger.debug("Converting csv file to list")
+            data = []
+            with open(file_path, newline="\n") as csv_file:
+                for row in csv.reader(csv_file, delimiter=","):
+                    lst = []
+                    for t in row:
+                        try:
+                            lst.append(int(t))
+                        except ValueError:
+                            lst.append(t)
+                            continue
+                    data.append(tuple(lst))
+            self.logger.info("File was converted")
+
+            return data
--- web/app/routes.py
+++ web/app/routes.py
@@ -10,16 +10,21 @@ from app.models import User
 from werkzeug.urls import url_parse
 import sqlite3
 
+import database
+
 
 # TODO: Убрать обновление страницы, если в этом нет нужды
 # TODO: Добавить валидацию для форм в таблице
+# TODO: Использовать свою database.py
+# TODO: Сделать разные логгеры для каждого из классов или файлов
+# TODO: Заменить str condition в database.py на tuple condition
 
 
 @flsk.route("/index", methods=["GET", "POST"])
 @login_required # Проверяем авторизовался ли пользователь
 def index():
-    db = sqlite3.connect("../data/database.db")
-    navig_data = list(db.execute("SELECT CAST(id as TEXT), name FROM fuel"))
+    db = database.DataBase("../data/database.db")
+    navig_data = list(db.select("name", "CAST(id as TEXT), name"))
 
     navig_trans_form = NavigationTransForm()
     navig_trans_form.names.choices = navig_data
@@ -37,179 +42,109 @@ def index():
     table_name = "trans"
 
     # Создаем view для того, чтобы в дальнейшем не повторять условия 
-    try:
-        db.execute("DROP VIEW IF EXISTS vtrans")
-        db.execute("""CREATE VIEW vtrans AS SELECT
-                    t.id, t.fuel_id, t.dtime, t.odometer, f.name, t.amount
-                    FROM trans t, fuel f WHERE t.fuel_id = f.id
-                    ORDER BY t.dtime""")
-    except sqlite3.Error as e:
-        flsk.logger.error(e)
-
-    try:
-        db.execute("DROP VIEW IF EXISTS vfuel")
-        db.execute("""CREATE VIEW vfuel AS SELECT
-                      id, name, price
-                      FROM fuel""")
-    except sqlite3.Error as e:
-        flsk.logger.error(e)
+    db.create_view("vtrans", "trans t, fuel f",
+                   "t.id, t.fuel_id, t.dtime, t.odometer, f.name, t.amount",
+                   "t.fuel_id = f.id", "t.dtime", re_create=True)
+    db.create_view("vfuel", "fuel", "id, name, price", re_create=True)
 
     # Проверяем, была ли нажата какая-то из submit кнопок в веб формах
-    if fuel_row_form.validate_on_submit() or trans_row_form.validate_on_submit() or navig_fuel_form.validate_on_submit() or navig_trans_form.validate_on_submit() or fuel_new_row_form.validate_on_submit() or trans_new_row_form.validate_on_submit():
+    if (fuel_row_form.validate_on_submit() 
+       or trans_row_form.validate_on_submit()
+       or navig_fuel_form.validate_on_submit()
+       or navig_trans_form.validate_on_submit()
+       or fuel_new_row_form.validate_on_submit()
+       or trans_new_row_form.validate_on_submit()):
         flsk.logger.debug("Index submit button was pressed")
         # Проверяем какая кнопка была нажата
         if trans_row_form.save_trans.data:
             # Если кнопка сохранения была нажата, то обновляем уже имеющуюся строку в таблице,
             # подстваляя новые значения
             flsk.logger.debug("Save button was pressed in the row of the trans table")
-            try:
-                db.execute("UPDATE trans" +
-                           " SET dtime = '" + str(trans_row_form.date.data) + "'" +
-                           ", odometer = " + str(trans_row_form.odometer.data) +
-                           ", fuel_id = " + str(trans_row_form.fuel_station.data) +
-                           ", amount = " + str(trans_row_form.gallon_count.data) + 
-                           " WHERE id = " + str(trans_row_form.id.data))
-                db.commit()
-                flsk.logger.info("Row with id = " + str(trans_row_form) + "from the trans table was updated")
-                flsk.logger.debug("With parameters:" +
-                                  "\n    dtime=" + str(trans_row_form.date.data) +
-                                  ",\n    odometer=" + str(trans_row_form.odometer.data) +
-                                  ",\n    fuel_id = " + str(trans_row_form.fuel_station.data) +
-                                  ",\n    amount = " + str(trans_row_form.gallon_count.data))
-            except sqlite3.Error as e:
-                flsk.logger.error(e)
+            db.update("trans", 
+                      ("dtime = '" + str(trans_row_form.date.data) + "'" +
+                       ", odometer = " + str(trans_row_form.odometer.data) +
+                       ", fuel_id = " + str(trans_row_form.fuel_station.data) +
+                       ", amount = " + str(trans_row_form.gallon_count.data)),
+                      "id = " + str(trans_row_form.id.data))
+            db.commit()
             table_name = "trans"
         elif fuel_row_form.save_fuel.data:
             # Если кнопка сохранения была нажата, то обновляем уже имеющуюся строку в таблице,
             # подстваляя новые значения
             flsk.logger.debug("Save button was pressed in the row of the fuel table")
-            try:
-                db.execute("UPDATE fuel" +
-                           " SET name = '" + str(fuel_row_form.name.data) + "'" +
-                           ", price = " + str(fuel_row_form.price.data) +
-                           " WHERE id = " + str(fuel_row_form.id.data))
-                db.commit()
-                flsk.logger.info("Row with id = " + str(trans_row_form) + "from the fuel table was updated")
-                flsk.logger.debug("With parameters:" +
-                                  "\n    name=" + str(fuel_row_form.name.data) +
-                                  ",\n    price=" + str(fuel_row_form.price.data))
-            except sqlite3.Error as e:
-                flsk.logger.error(e)
+            db.update("fuel",
+                      "name = '" + str(fuel_row_form.name.data) + "'" +
+                      ", price = " + str(fuel_row_form.price.data),
+                      "id = " + str(fuel_row_form.id.data))
+            db.commit()
             table_name = "fuel"
 
         elif navig_trans_form.trans_allow.data:
             # Если кнопка подтвержедения в навигационной форме была нажата,
             # то Создаем новую view
             flsk.logger.debug("Allow button was pressed in the trans navigation")
-            command = ("""CREATE VIEW vtrans AS SELECT
-                          t.id,  t.fuel_id, t.dtime, t.odometer, f.name, t.amount
-                          FROM trans t, fuel f WHERE t.fuel_id = f.id""" +
-                       " AND t.dtime > '" + str(navig_trans_form.start_date.data) + "'" +
-                       " AND t.dtime < '" + str(navig_trans_form.end_date.data) + "'")
+            condition = ("t.fuel_id = f.id" +
+                         " AND t.dtime > '" + str(navig_trans_form.start_date.data) + "'" +
+                         " AND t.dtime < '" + str(navig_trans_form.end_date.data) + "'")
             if len(navig_trans_form.names.data) == 1:
-                command += " AND t.fuel_id == " + str(navig_trans_form.names.data[0])
+                condition += " AND t.fuel_id == " + str(navig_trans_form.names.data[0])
             elif len(navig_trans_form.names.data) != 0:
-                command += " AND t.fuel_id in " + str(tuple(navig_trans_form.names.data))
-            command += " ORDER BY dtime"
-            try:
-                db.execute("DROP VIEW IF EXISTS vtrans")
-                db.execute(command)
-                flsk.logger.info("Trans view was created")
-                flsk.logger.debug("With paremeters:"
-                                  "\n    start_date=" + str(navig_trans_form.start_date.data) +
-                                  ",\n    end_date=" + str(navig_trans_form.end_date.data) +
-                                  ",\n    fuel_id in [" + str(navig_trans_form.names.data) + "]")
-            except sqlite3.Error as e:
-                flsk.logger.error(e)
+                condition += " AND t.fuel_id in " + str(tuple(navig_trans_form.names.data))
+            db.create_view("vtrans", "trans t, fuel f",
+                           "t.id,  t.fuel_id, t.dtime, t.odometer, f.name, t.amount",
+                           condition, "dtime", re_create=True)
+            db.commit()
             table_name = "trans"
         elif navig_fuel_form.fuel_allow.data:
             # Если кнопка подтвержедения в навигационной форму была нажата,
             # то создаем новую view
             flsk.logger.debug("Allow button was pressed in the fuel navigation")
-            command = ("""CREATE VIEW vfuel AS SELECT
-                          id, name, price
-                          FROM fuel
-                          WHERE price > """ + str(navig_fuel_form.start_price.data) +
-                       " AND price < " + str(navig_fuel_form.end_price.data))
-            table_name = "fuel"
-            try:
-                db.execute("DROP VIEW IF EXISTS vfuel")
-                db.execute(command)
-                flsk.logger.info("Fuel view was created")
-                flsk.logger.debug("With paremeters:"
-                                  "\n    price=" + str(navig_fuel_form.start_price.data) +
-                                  ",\n    end_date=" + str(navig_fuel_form.end_price.data))
-            except sqlite3.Error as e:
-                flsk.logger.error(e)
+            db.create_view("vfuel", "fuel", "id, name, price",
+                           ("price > " + str(navig_fuel_form.start_price.data) +
+                            " AND price < " + str(navig_fuel_form.end_price.data)),
+                           re_create=True)
+            db.commit()
             table_name = "fuel"
 
         elif trans_row_form.delete_trans.data:
             # Если была нажата кнопка удаления в веб форме строки в таблицу,
             # то удаляем строку в которой id из таблицы будет совподать с id из веб формы
             flsk.logger.debug("Delete button was pressed in the row of the trans table")
-            try:
-                db.execute("DELETE FROM trans WHERE id = " + str(trans_row_form.id.data))
-                db.commit()
-                flsk.logger.info("Deleted row with id = " + str(trans_row_form.id.data) + "from trans table")
-            except sqlite3.Error as e:
-                flsk.logger.error(e)
+            db.delete("trans", "id = " + str(trans_row_form.id.data))
+            db.commit()
             table_name = "trans"
         elif fuel_row_form.delete_fuel.data:
             # Если была нажата кнопка удаления в веб форме строки в таблицу,
             # то удаляем строку в которой id из таблицы будет совподать с id из веб формы
             flsk.logger.debug("Delete button was pressed in the row of the fuel table")
-            table_name = "fuel"
-            try:
-                db.execute("DELETE FROM fuel WHERE id = " + str(fuel_row_form.id.data))
-                db.commit()
-                flsk.logger.info("Deleted row with id = " + str(fuel_row_form.id.data) + "from fuel table")
-            except sqlite3.Error as e:
-                flsk.logger.error(e)
+            db.delete("fuel", "id = " + str(fuel_row_form.id.data))
+            db.commit()
             table_name = "fuel"
 
         elif trans_new_row_form.add_trans.data:
             # Если в веб форме новой строки, была нажата кнопка добавления,
             # добавляем новую строку с параментрами из веб форм
             flsk.logger.debug("Add button was pressed in the new row of the trans table")
-            try:
-                db.execute("INSERT INTO trans(dtime, odometer, fuel_id, amount) VALUES (" +
-                           "'" + str(trans_new_row_form.date.data) + "'" +
-                           ", " + str(trans_new_row_form.odometer.data) + 
-                           ", " + str(trans_new_row_form.fuel_station.data) +
-                           ", " + str(trans_new_row_form.gallon_count.data) + ")")
-                db.commit()  
-                flsk.logger.info("Added new row to trans table")
-            except sqlite3.Error as e:
-                flsk.logger.error(e)
+            db.insert("trans", "dtime, odometer, fuel_id, amount",
+                      ("'" + str(trans_new_row_form.date.data) + "'" +
+                       ", " + str(trans_new_row_form.odometer.data) + 
+                       ", " + str(trans_new_row_form.fuel_station.data) +
+                       ", " + str(trans_new_row_form.gallon_count.data)))
             table_name = "trans"
         elif fuel_new_row_form.add_fuel.data:
             # Если в веб форме новой строки, была нажата кнопка добавления,
             # добавляем новую строку с параментрами из веб форм
             flsk.logger.debug("Add button was pressed in the new row of the fuel table")
-            try:
-                db.execute("INSERT INTO fuel(name, price) VALUES (" +
-                           "'" + str(fuel_new_row_form.name.data) + "'" +
-                           ", " + str(fuel_new_row_form.price.data) + ")")
-                db.commit()
-                flsk.logger.info("Added new row to fuel table")
-            except sqlite3.Error as e:
-                flsk.logger.error(e)
+            db.insert("fuel", "name, price", 
+                      ("'" + str(fuel_new_row_form.name.data) + "'" +
+                       ", " + str(fuel_new_row_form.price.data)))
             table_name = "fuel"
     # Достаем данные о заправлках из базы данных
     flsk.logger.debug("Selecting data from trans view")
-    try:
-        trans_data = db.execute("""SELECT id, fuel_id, dtime, odometer, name, amount
-                                   FROM vtrans""")
-        flsk.logger.info("Data was successfully received from trans view")
-    except sqlite3.Error as e:
-        flsk.logger.error(e)
+    trans_data = db.select("vtrans",
+                           "id, fuel_id, dtime, odometer, name, amount")
     flsk.logger.debug("Selecting data from fuel view")
-    try:
-        fuel_data = db.execute("""SELECT id, name, price
-                                  FROM vfuel""")
-        flsk.logger.info("Data was successfully received from fuel view")
-    except sqlite3.Error as e:
-        flsk.logger.error(e)
+    fuel_data = db.select("vfuel", "id, name, price")
 
     # Обновляем страницу
     flsk.logger.debug("Rendering index page")
