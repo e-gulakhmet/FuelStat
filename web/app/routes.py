d54affabdcb3b7aae52d3a3fc5dbd7f70cd301a9
who-man-tech <enes515254@gmail.com>
2021-02-07 17:05:38 +0200

    Fix commiting changed database row

--- web/app/routes.py
+++ web/app/routes.py
@@ -16,10 +16,6 @@ from app.database import DataBase
 
 
 # TODO: Убрать обновление страницы, если в этом нет нужды
-# TODO: Починить обновление строки, которая была создана в браузере
-#       (удаляется после изменения данных)
-# TODO: Починить сортировку при получении данных из базы данных
-#       (строка с меньшим пробегом стоит после строк с большим пробегом, при одной дате)
 
 
 @flsk.route("/index", methods=["GET", "POST"])
@@ -145,6 +141,7 @@ def index():
                        ", " + str(trans_new_row_form.odometer.data) + 
                        ", " + str(trans_new_row_form.fuel_station.data) +
                        ", " + str(trans_new_row_form.gallon_count.data)))
+            db.commit()
             table_name = "trans"
         
         elif fuel_new_row_form.validate() and fuel_new_row_form.add_fuel.data:
@@ -154,6 +151,7 @@ def index():
             db.insert("fuel", "name, price", 
                       ("'" + str(fuel_new_row_form.name.data) + "'" +
                        ", " + str(fuel_new_row_form.price.data)))
+            db.commit()
             table_name = "fuel"
         
         elif report_form.validate() and report_form.get_report.data:
@@ -179,137 +177,12 @@ def index():
                       __file__.replace("web/app/routes.py", "main.py") +
                       report_param)
             return send_file(__file__.replace("web/app/routes.py", "data/report.pdf"), attachment_filename="report.pdf")
-        
 
-
-    # if (fuel_row_form.validate_on_submit() 
-    #    or trans_row_form.validate_on_submit()
-    #    or navig_fuel_form.validate_on_submit()
-    #    or navig_trans_form.validate_on_submit()
-    #    or fuel_new_row_form.validate_on_submit()
-    #    or trans_new_row_form.validate_on_submit()):
-    #     logger.debug("Index submit button was pressed")
-    #     print(fuel_row_form.validate_on_submit())
-    #     print(trans_row_form.validate_on_submit())
-    #     print(navig_fuel_form.validate_on_submit())
-    #     print(navig_trans_form.validate_on_submit())
-    #     print(fuel_new_row_form.validate_on_submit())
-    #     print(trans_new_row_form.validate_on_submit())
-    #     # Проверяем какая кнопка была нажата
-    #     if trans_row_form.save_trans.data:
-    #         # Если кнопка сохранения была нажата, то обновляем уже имеющуюся строку в таблице,
-    #         # подстваляя новые значения
-    #         logger.debug("Save button was pressed in the row of the trans table")
-    #         db.update("trans", 
-    #                   ("dtime = '" + str(trans_row_form.date.data) + "'" +
-    #                    ", odometer = " + str(trans_row_form.odometer.data) +
-    #                    ", fuel_id = " + str(trans_row_form.fuel_station.data) +
-    #                    ", amount = " + str(trans_row_form.gallon_count.data)),
-    #                   "id = " + str(trans_row_form.id.data))
-    #         db.commit()
-    #         table_name = "trans"
-    #     elif fuel_row_form.save_fuel.data:
-    #         # Если кнопка сохранения была нажата, то обновляем уже имеющуюся строку в таблице,
-    #         # подстваляя новые значения
-    #         logger.debug("Save button was pressed in the row of the fuel table")
-    #         db.update("fuel",
-    #                   "name = '" + str(fuel_row_form.name.data) + "'" +
-    #                   ", price = " + str(fuel_row_form.price.data),
-    #                   "id = " + str(fuel_row_form.id.data))
-    #         db.commit()
-    #         table_name = "fuel"
-
-    #     elif navig_trans_form.trans_allow.data:
-    #         # Если кнопка подтвержедения в навигационной форме была нажата,
-    #         # то Создаем новую view
-    #         logger.debug("Allow button was pressed in the trans navigation")
-    #         condition = ("t.fuel_id = f.id" +
-    #                      " AND t.dtime > '" + str(navig_trans_form.start_date.data) + "'" +
-    #                      " AND t.dtime < '" + str(navig_trans_form.end_date.data) + "'" +
-    #                      " AND t.odometer > " + str(navig_trans_form.start_odometer.data) +
-    #                      " AND t.odometer < " + str(navig_trans_form.end_odometer.data))
-    #         if len(navig_trans_form.names.data) == 1:
-    #             condition += " AND t.fuel_id == " + str(navig_trans_form.names.data[0])
-    #         elif len(navig_trans_form.names.data) != 0:
-    #             condition += " AND t.fuel_id in " + str(tuple(navig_trans_form.names.data))
-    #         db.create_view("vtrans", "trans t, fuel f",
-    #                        "t.id,  t.fuel_id, t.dtime, t.odometer, f.name, t.amount",
-    #                        condition, "dtime", re_create=True)
-    #         db.commit()
-    #         table_name = "trans"
-    #     elif navig_fuel_form.fuel_allow.data:
-    #         # Если кнопка подтвержедения в навигационной форму была нажата,
-    #         # то создаем новую view
-    #         logger.debug("Allow button was pressed in the fuel navigation")
-    #         db.create_view("vfuel", "fuel", "id, name, price",
-    #                        ("price > " + str(navig_fuel_form.start_price.data) +
-    #                         " AND price < " + str(navig_fuel_form.end_price.data)),
-    #                        re_create=True)
-    #         db.commit()
-    #         table_name = "fuel"
-
-    #     elif trans_row_form.delete_trans.data:
-    #         # Если была нажата кнопка удаления в веб форме строки в таблицу,
-    #         # то удаляем строку в которой id из таблицы будет совподать с id из веб формы
-    #         logger.debug("Delete button was pressed in the row of the trans table")
-    #         db.delete("trans", "id = " + str(trans_row_form.id.data))
-    #         db.commit()
-    #         table_name = "trans"
-    #     elif fuel_row_form.delete_fuel.data:
-    #         # Если была нажата кнопка удаления в веб форме строки в таблицу,
-    #         # то удаляем строку в которой id из таблицы будет совподать с id из веб формы
-    #         logger.debug("Delete button was pressed in the row of the fuel table")
-    #         db.delete("fuel", "id = " + str(fuel_row_form.id.data))
-    #         db.commit()
-    #         table_name = "fuel"
-
-    #     elif trans_new_row_form.add_trans.data:
-    #         # Если в веб форме новой строки, была нажата кнопка добавления,
-    #         # добавляем новую строку с параментрами из веб форм
-    #         logger.debug("Add button was pressed in the new row of the trans table")
-    #         db.insert("trans", "dtime, odometer, fuel_id, amount",
-    #                   ("'" + str(trans_new_row_form.date.data) + "'" +
-    #                    ", " + str(trans_new_row_form.odometer.data) + 
-    #                    ", " + str(trans_new_row_form.fuel_station.data) +
-    #                    ", " + str(trans_new_row_form.gallon_count.data)))
-    #         table_name = "trans"
-    #     elif fuel_new_row_form.add_fuel.data:
-    #         # Если в веб форме новой строки, была нажата кнопка добавления,
-    #         # добавляем новую строку с параментрами из веб форм
-    #         logger.debug("Add button was pressed in the new row of the fuel table")
-    #         db.insert("fuel", "name, price", 
-    #                   ("'" + str(fuel_new_row_form.name.data) + "'" +
-    #                    ", " + str(fuel_new_row_form.price.data)))
-    #         table_name = "fuel"
-
-    #     elif report_form.get_report.data:
-    #         # Если кнопка подтвержедения в навигационной форме была нажата,
-    #         # то Создаем новую view
-    #         logger.debug("Allow button was pressed in the report")
-    #         report_param = (" --report" +
-    #                         " --startdate " + str(report_form.start_date.data) +
-    #                         " --enddate " + str(report_form.end_date.data) +
-    #                         " --startodometer " + str(report_form.start_odometer.data) +
-    #                         " --endodometer " + str(report_form.end_odometer.data))
-    #         if report_form.names.data is not None:
-    #             for i in report_form.names.data:
-    #                 for station in stations_info:
-    #                     if station[0] == i:
-    #                         report_param += " --gasname " + station[1]
-    #                         break
-    #         if report_form.show_statistic.data:
-    #             report_param += " --statistic"
-    #         if report_form.show_table.data:
-    #             report_param += " --info"
-    #         os.system("python " +
-    #                   __file__.replace("web/app/routes.py", "main.py") +
-    #                   report_param)
-    #         return send_file(__file__.replace("web/app/routes.py", "data/report.pdf"), attachment_filename="report.pdf")
     # Достаем данные о заправлках из базы данных
     logger.debug("Selecting data from trans view")
     trans_data = db.select("vtrans",
                            "id, fuel_id, dtime, odometer, name, amount",
-                           order_by="dtime")
+                           order_by="odometer")
     logger.debug("Selecting data from fuel view")
     fuel_data = db.select("vfuel", "id, name, price")
 
